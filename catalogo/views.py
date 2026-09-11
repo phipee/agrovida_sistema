de35b310736d5b233f13109e2b8b74f846ba2756
Laboratorio 320 <AFT_LAB320@inacap.cl>
2026-09-11 17:25:39 -0300

    cuarto commit : creación de la función de vista en views.py

--- catalogo/views.py
+++ catalogo/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+def mostrar_productos(render):
+    productos = [
+    {"nombre": "Fertilizante NPK 15-15-15", "categoria": "Fertilizantes", "precio": 18990, "stock": 120},
+    {"nombre": "Semilla de Maíz Híbrido", "categoria": "Semillas", "precio": 5990, "stock": 340},
+    {"nombre": "Herbicida Glifosato 20L", "categoria": "Agroquímicos", "precio": 45990, "stock": 25},
+    contexto = {'catalogo': catalogo}
+
+
+    ]
+    return render (request, 'index.html', contexto  )
+
+
+    return render(request, 'pagina.html', contexto)
 # Create your views here.
